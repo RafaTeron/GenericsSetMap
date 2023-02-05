a37b541eb142a2626218a31af88069c35f32c934
Rafael Abreu <rafael_sabreu@hotmail.com>
2023-02-04 22:43:25 -0300

    Last commit

--- Exercicio04/application/Program09.java
+++ Exercicio04/application/Program09.java
@@ -3,11 +3,9 @@ package application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
-
-import entities.Candidato;
 
 public class Program09 {
 
@@ -15,12 +13,12 @@ public class Program09 {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		Map<String, Integer> quant = new LinkedHashMap<>();
+		
 		System.out.print("Enter file full path: ");
 		String path = sc.nextLine();
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-			
-			Map<Candidato, Integer> map = new HashMap<>();
 			
 			String line = br.readLine();
 			while (line != null) {
@@ -28,14 +26,21 @@ public class Program09 {
 				String[] fields = line.split(",");
 				String nome = fields[0];
 				int votos = Integer.parseInt(fields[1]);
-				Candidato candidato = new Candidato(nome, votos);
-				map.add(candidato);
 				
-				line = br.readLine();			
-		
+				if (quant.containsKey(nome)) {
+					int votesSoFar = quant.get(nome);
+					quant.put(nome, votos + votesSoFar);
+				}
+				else {
+					quant.put(nome, votos);
+				}
+				
+				line = br.readLine();
 			}
 			
-			System.out.println("Total users: " + set.size());
+			for (String key : quant.keySet()) {
+				System.out.println(key + ": " + quant.get(key));
+			}
 		
 		
 		} catch (IOException e) {
